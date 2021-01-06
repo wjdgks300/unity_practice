0f60037be3811ccb86df1cfb85d161757aaa19fc
leejeonghan <wjdgks300@naver.com>
2021-01-06 16:40:10 +0900

    second

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,33 +8,99 @@ public class Player : MonoBehaviour
     float hAxis;
     float vAxis;
     bool wDown;
+    bool jDown;
+
+    bool isjump;
+    bool isDodge;
 
     Vector3 moveVec;
+    Vector3 dodgeVec;
 
+    Rigidbody rigid;
     Animator anim;
 
     
     // Start is called before the first frame update
     void Awake()
     {
+        rigid = GetComponent<Rigidbody>();
         anim = GetComponentInChildren<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
+
+        GetInput();
+        Move();
+        Turn();
+        Jump();
+        Dodge();
+    }
+    
+    void GetInput()
+    {
         hAxis = Input.GetAxisRaw("Horizontal");
         vAxis = Input.GetAxisRaw("Vertical");
         wDown = Input.GetButton("walk");
+        jDown = Input.GetButton("Jump");
+    }
 
+    void Move()
+    {
         moveVec = new Vector3(hAxis, 0, vAxis).normalized;
 
+        if (isDodge)
+        {
+            moveVec = dodgeVec;
+        }
         transform.position += moveVec * speed * (wDown ? 0.3f : 1f) * Time.deltaTime;
 
         anim.SetBool("isRun", moveVec != Vector3.zero);
         anim.SetBool("isWalk", wDown);
+    }
 
-
+    void Turn()
+    {
         transform.LookAt(transform.position + moveVec);
+    }
+
+    void Jump()
+    {
+        if (jDown && moveVec == Vector3.zero && !isjump && !isDodge)
+        {
+            rigid.AddForce(Vector3.up * 25, ForceMode.Impulse);
+            anim.SetBool("isJump", true);
+            anim.SetTrigger("doJump");
+            isjump = true;
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Floor")
+        {
+            anim.SetBool("isJump", false);
+            isjump = false;
+        }   
+    }
+
+
+    void Dodge()
+    {
+        if (jDown && moveVec != Vector3.zero && !isjump &&!isDodge)
+        {
+            dodgeVec = moveVec;
+            speed *= 2;
+            anim.SetTrigger("doDodge");
+            isDodge = true;
+
+            Invoke("DodgeOut", 0.5f);
+        }
+    }
+    void DodgeOut() {
+        speed *= 0.5f;
+        isDodge = false;
+    
     }
 }
